4408b8bd05f9d5645a72388d4e8c198005779523
acs1dev <126028939+acs1dev@users.noreply.github.com>
2025-06-10 18:52:26 +0300

    Update AssemblyInfo.cs

--- Web Design Project 1/Properties/AssemblyInfo.cs
+++ Web Design Project 1/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("58b1b657-0451-4c5d-bc28-de64b36e9351")]
+[assembly: Guid("auto-generated")]
 
 // Version information for an assembly consists of the following four values:
 //
